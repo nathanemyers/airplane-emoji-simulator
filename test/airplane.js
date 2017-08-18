c3ff52b32a0718ee526dedd77a98b174b1ff45f2
Nathan Myers <nathanemyers@gmail.com>
2017-08-18 15:37:07 -0500

    teaching mocha about es6 ☕️

---
+++ test/airplane.js
@@ -0,0 +1,16 @@
+var assert = require('assert');
+import "../app/airplane.js"
+
+describe('Airplane Functions', function() {
+  describe('Fuel Stuff', function() {
+    it('should burn fuel', function() {
+    });
+
+    it('should crash when out of fuel', function() {
+    });
+
+    it('should start topped off', function() {
+    });
+
+  });
+});
