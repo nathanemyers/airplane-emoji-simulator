8f13756f95ef2d0414e7724dfe8586fb9e5fa6d4
Nathan Myers <nathanemyers@gmail.com>
2017-08-18 18:47:13 -0500

    let's just use a switch statement in here

--- app/airplane.js
+++ app/airplane.js
@@ -1,18 +1,18 @@
 let airplane = {}
 
 export const PLANE_STATUS = {
-  flight: "In Flight",
-  taxi: "Taxing",
-  takeoff: "Taking Off",
-  landing: "Landing",
-  crash:  "Crashed",
+  FLIGHT: "In Flight",
+  TAXI: "Taxi",
+  TAKEOFF: "Taking Off",
+  LANDING: "Landing",
+  CRASH: "Crashed",
 }
 
 export class Airplane {
-  constructor(fuel=100) {
+  constructor(fuel=100, status=PLANE_STATUS.TAXI) {
     this._fuel_capacity = fuel
     this._fuel = fuel
-    this._status = PLANE_STATUS.taxi
+    this._status = status
   }
 
   get fuel() {
@@ -20,10 +20,33 @@ export class Airplane {
   }
 
   get status() {
-    return this.status
+    return this._status
+  }
+
+  takeoff() {
+    if (this._status = PLANE_STATUS.TAXI) {
+      this._status = PLANE_STATUS.TAKEOFF
+    }
   }
 
   tick() {
-    this._fuel = this._fuel - 1
+    switch (this._status) {
+      case PLANE_STATUS.FLIGHT:
+        this._fuel = this._fuel - 1
+        if (this._fuel === 0) {
+          this._status = PLANE_STATUS.CRASH
+        }
+        break
+      case PLANE_STATUS.TAXI:
+        break
+      case PLANE_STATUS.TAKEOFF:
+        this._status = PLANE_STATUS.FLIGHT
+        break
+      case PLANE_STATUS.LANDING:
+        this._status = PLANE_STATUS.TAXI
+        break
+      case PLANE_STATUS.CRASH:
+        break
+    }
   }
 }
--- test/airplane.js
+++ test/airplane.js
@@ -1,19 +1,52 @@
 var assert = require('assert');
 import { Airplane, PLANE_STATUS } from "../app/airplane.js"
 
-describe('Airplane Functions', function() {
-  describe('Fuel Stuff', function() {
-    it('should burn fuel', function() {
-      let plane = new Airplane(10)
-      plane.tick()
-      assert.equal(plane.fuel, 9);
-    });
+describe('Airplanes', function() {
+  it('should burn fuel', function() {
+    let plane = new Airplane(10, PLANE_STATUS.FLIGHT)
+    plane.tick()
+    assert.equal(plane.fuel, 9);
+  });
 
-    it('should crash when out of fuel', function() {
-    });
+  it('should crash when out of fuel', function() {
+    let plane = new Airplane(2, PLANE_STATUS.FLIGHT)
+    plane.tick()
+    assert.equal(plane.status, PLANE_STATUS.FLIGHT);
+    plane.tick()
+    assert.equal(plane.status, PLANE_STATUS.CRASH);
+  });
 
-    it('should start topped off', function() {
-    });
+  it('only burns fuel while airbourne', function() {
+    let flight_plane = new Airplane(10, PLANE_STATUS.FLIGHT)
+    let taxi_plane = new Airplane(10, PLANE_STATUS.TAXI)
+    let takeoff_plane = new Airplane(10, PLANE_STATUS.TAKEOFF)
+    let landing_plane = new Airplane(10, PLANE_STATUS.LANDING)
+    let crash_plane = new Airplane(10, PLANE_STATUS.CRASH)
 
+    flight_plane.tick()
+    taxi_plane.tick()
+    takeoff_plane.tick()
+    landing_plane.tick()
+    crash_plane.tick()
+
+    assert.equal(flight_plane.fuel, 9);
+    assert.equal(taxi_plane.fuel, 10);
+    assert.equal(takeoff_plane.fuel, 10);
+    assert.equal(landing_plane.fuel, 10);
+    assert.equal(crash_plane.fuel, 10);
+  });
+
+  it('should start topped off', function() {
+    let plane = new Airplane(10)
+    assert.equal(plane.fuel, 10);
+  });
+
+  it('can takeoff', function() {
+    let plane = new Airplane(10)
+    assert.equal(plane.status, PLANE_STATUS.TAXI);
+    plane.takeoff()
+    assert.equal(plane.status, PLANE_STATUS.TAKEOFF);
+    plane.tick()
+    assert.equal(plane.status, PLANE_STATUS.FLIGHT);
   });
 });
