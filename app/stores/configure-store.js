ccd7afdd3a9c2e5b758f246f36f0d3b27791965e
Nathan Myers <nathanemyers@gmail.com>
2017-09-14 15:52:06 -0500

    updating lib code

--- app/stores/configure-store.js
+++ app/stores/configure-store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import createLogger from 'redux-logger';
+import { createLogger } from 'redux-logger';
 import rootReducer from '../redux/reducers/index';
 
 const logger = createLogger({
