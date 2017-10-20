83f10bc47bf567bacb2dc509ee7ac6b3e1f17a8e
Nathan Myers <nathanemyers@gmail.com>
2017-10-20 16:45:51 -0500

    stubbing out tests

--- test/airplane.js
+++ test/airplane.js
@@ -1,66 +1,33 @@
-var assert = require('assert');
-import { Airplane, PLANE_STATUS } from "../app/engine/airplane.js"
+import assert from 'assert'
+import { Airplane, PLANE_STATUS } from "app/engine/airplane.js"
+
+import { configureMockStore } from 'redux-mock-store';
+import simulationRedux from 'redux/reducers/index.js'
+
+const store = configureMockStore(simulationRedux)
 
 describe('Airplanes', function() {
-  //it('should burn fuel', function() {
-    //let plane = new Airplane(10, 20, PLANE_STATUS.FLIGHT)
-    //plane.tick()
-    //assert.equal(plane.fuel, 9);
-  //});
+  it('can be created', () => {
+    store.dispatch(createAirplane({
+      name: 'tom',
+    }))
 
-  //it('should crash when out of fuel', function() {
-    //let plane = new Airplane(2, 20, PLANE_STATUS.FLIGHT)
-    //plane.tick()
-    //assert.equal(plane.status, PLANE_STATUS.FLIGHT);
-    //plane.tick()
-    //assert.equal(plane.status, PLANE_STATUS.CRASH);
-  //});
+    const state = store.getState()
 
-  //it('only burns fuel while airbourne', function() {
-    //let flight_plane = new Airplane(10, 20, PLANE_STATUS.FLIGHT)
-    //let taxi_plane = new Airplane(10, 20, PLANE_STATUS.TAXI)
-    //let takeoff_plane = new Airplane(10, 20, PLANE_STATUS.TAKEOFF)
-    //let landing_plane = new Airplane(10, 20, PLANE_STATUS.LANDING)
-    //let crash_plane = new Airplane(10, 20, PLANE_STATUS.CRASH)
+  })
+  it('burns fuel in flight', () => {
 
-    //flight_plane.tick()
-    //taxi_plane.tick()
-    //takeoff_plane.tick()
-    //landing_plane.tick()
-    //crash_plane.tick()
+  })
+  it('can takeoff', () => {
 
-    //assert.equal(flight_plane.fuel, 9);
-    //assert.equal(taxi_plane.fuel, 10);
-    //assert.equal(takeoff_plane.fuel, 10);
-    //assert.equal(landing_plane.fuel, 10);
-    //assert.equal(crash_plane.fuel, 10);
-  //});
+  })
+  it('can land', () => {
 
-  //it('should start topped off', function() {
-    //let plane = new Airplane(10)
-    //assert.equal(plane.fuel, 10);
-  //});
+  })
+  it('will crash when it runs out of fuel', () => {
 
-  //it('travels distance while in flight', function() {
-    //let plane = new Airplane(10, 20, PLANE_STATUS.FLIGHT)
-    //plane.tick()
-    //assert.equal(plane.eta, 19);
-  //});
+  })
+  it('will taxi', () => {
 
-  //it('does not go negative distance', function() {
-    //let plane = new Airplane(10, 1, PLANE_STATUS.FLIGHT)
-    //plane.tick()
-    //assert.equal(plane.eta, 0);
-    //plane.tick()
-    //assert.equal(plane.eta, 0);
-  //});
-
-  //it('can takeoff', function() {
-    //let plane = new Airplane(10)
-    //assert.equal(plane.status, PLANE_STATUS.TAXI);
-    //plane.takeoff()
-    //assert.equal(plane.status, PLANE_STATUS.TAKEOFF);
-    //plane.tick()
-    //assert.equal(plane.status, PLANE_STATUS.FLIGHT);
-  //});
+  })
 });
