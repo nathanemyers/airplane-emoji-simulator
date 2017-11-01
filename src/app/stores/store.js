a86ee2d7c254c8eb75e2219dfe32234570240153
Nathan Myers <nathanemyers@gmail.com>
2017-11-01 15:01:26 -0500

    removing cruft

--- src/app/stores/store.js
+++ src/app/stores/store.js
@@ -1,11 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
 import simulationApp from 'redux/reducers/index.js'
 
 const store = createStore(
   simulationApp,
-  applyMiddleware(thunk),
   applyMiddleware(logger),
 );
 
